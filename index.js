bc5d7c05d4cf024f7653c3d1a67e21d2ac1f58bb
Sean Conaty <anxiolydiot@Seans-MacBook-Pro.local>
2016-01-31 18:43:33 -0500

    working on constructing the zoo object

--- index.js
+++ index.js
@@ -22,5 +22,28 @@ connection.connect(function(err){
 console.log('connected as id ' + connection.threadId);
     });
 
+var zoo = {
+  welcome : function(){
+    console.log("Welcome to the Zoo And Friends App~")
+  },
+  menu: function(){
+    console.log(
+      'Enter (A): ------> to Add a new animal to the Zoo' +'/r/n'+
+      'Enter (U): ------> to Update info on an animal in the Zoo' +'/r/n'+
+      'Enter (V): ------> to Visit the animals in the Zoo' +'/r/n'+
+      'Enter (D): ------> to Adopt an animal from the Zoo' +'/r/n'+
+      'Enter (Q): ------> to Quit and exit the Zoo' +'/r/n'
+    )
+
+
+
+  },
+  add: function(inputscope){
+    var currentScope === input_scope;
+    console.log('To add an animal to the zoo please fill out the following form for us!');
+
+  }
+}
+
 
 connection.end();
